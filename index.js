5b03749c4ec905577c924a47ee358b4ba014ef8e
Lady Trell <antrellkent@gmail.com>
2022-04-27 23:45:36 -0400

    initial file checkin

---
+++ index.js
@@ -0,0 +1,4 @@
+
+const { employeeQuestions, managerQuestion, githubQuestion, menuQuestion, schoolQuestion } = require('./lib/Questions');
+const generatePage = require('./src/page-template.js');
+const inquirer =  require('inquirer');
